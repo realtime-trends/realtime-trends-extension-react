3d10c842dc4517e47b22b95ee1cd0437394a8e61
Changho Park <pch4463@gmail.com>
2021-08-07 05:18:35 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   app.use(
 		'/api',
 		createProxyMiddleware({
-			target: process.env.REACT_APP_BACKEND_PROXY,
+			target: process.env.REACT_APP_API_PROXY,
 			changeOrigin: true,
 		}))
 }
