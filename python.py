96833b779a20eaf52c4cb22cf366cbf6f0076630
Changho Park <sayh4463@hanyang.ac.kr>
2021-10-04 11:03:08 +0900

    Delete python.py

--- python.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-r = requests.post('https://www.saramin.co.kr/zf_user/tools/spell-check', data={
-            'content': "아버지가방에들어가신다."
-        })
-
-print(json.loads(r.text))
