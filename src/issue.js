6b7212354ff2b6cad9d875ae3892152e5d682865
Changho Park <sayh4463@hanyang.ac.kr>
2021-12-15 01:22:15 +0900

    Update: change method crawl rank data from zum.

--- src/issue.js
+++ src/issue.js
@@ -40,10 +40,10 @@ axios.defaults.headers = {
 async function getKeywordsByEngine(engine) {
   var keywords = {};
   if (engine.localeCompare('zum') === 0) {
-    await axios.get('https://issue.zum.com/').then((data) => {
+    await axios.get('https://search.zum.com/search.zum').then((data) => {
       const $ = cheerio.load(data.data);
-      $('#issueKeywordList > li > .cont > .word').each((index, item) => {
-        const keyword = item.children[0].data;
+      $('#issue_wrap > ul > li > div > a:nth-child(1) > span.txt').each((index, item) => {
+        const keyword = item.innerText;
         keywords[keyword] = WEIGHTS[index] * ENGINE_BIAS[engine];
       });
     });
